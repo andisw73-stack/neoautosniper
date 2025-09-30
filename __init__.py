c37d487f024024c0ebacc49e06e472a53b9d6de5
andisw73-stack <andisw73@gmail.com>
2025-09-30 09:09:04 +0200

    Clean start

--- __init__.py
+++
@@ -1,8 +0,0 @@
-
-from .strat_dexs import DexScreenerStrategy
-# from .strat_gmgn import GmgnStrategy
-
-STRATEGIES = {
-    DexScreenerStrategy.name: DexScreenerStrategy,
-    # "gmgn": GmgnStrategy,
-}
